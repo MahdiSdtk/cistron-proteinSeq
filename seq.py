16e5ed5443935134ec4def1a0029344ea0565f9c
Mahdi Saadatkhah <136104679+MahdiSdtk@users.noreply.github.com>
2023-06-19 23:01:09 +0330

    dataset

--- seq.py
+++ seq.py
@@ -1,6 +1,9 @@
 import numpy as np
 import json
+import csv
+import pathlib
 import colorama
+
 
 
 def simpleError():
@@ -8,12 +11,17 @@ def simpleError():
 
 
 def transcript(sequence):
+    sequence = sequence.strip()
     sequence = sequence.uppercase()
     sequence = sequence.replace('A', '1')
-    sequence = sequence.replace('T', '2')
-    sequence = sequence.replace('U', '2')
+    if sequence.find('T') is True: sequence = sequence.replace('T', '2')
+    else: sequence = sequence.replace('U', '2')
     sequence = sequence.replace('C', '3')
     sequence = sequence.replace('G', '4')
+    sequence = sequence.replace('1', 'U')
+    sequence = sequence.replace('2', 'A')
+    sequence = sequence.replace('3', 'G')
+    sequence = sequence.replace('4', 'C')
     print(sequence)
     #  incomplete numbers to mRNA
 
@@ -24,12 +32,13 @@ with open('pro-letter.json', mode='r', encoding='utf8') as file:
 
 while(True):
     lst = np.array([])  #  what is this for?:o
-    input1 = input('\ninput a cistron sequence.\n1)from a file\n2)enter it as a text\n')
+    input1 = input('\ninput a cistron sequence.\n1)from a file\n2)enter it as a text\n3)locate the root folder\n')
     if input1 == 1:  # seq from  file input (could be a txt, fasta or ...)
-        print('allocate the intended sequence')
-        file_loc = input(': ')
+        print('locate the intended sequence')
+        seq_loc = pathlib.Path(input(': '))
+
         try:
-            with open(file_loc, mode='r', encoding='utf8') as file:
+            with open(seq_loc, mode='r', encoding='utf8') as file:
                 file = file.read()
                 # incomplete sequence2str
         # incomplete seq str to mRNA then protein
@@ -39,6 +48,21 @@ while(True):
     elif input1 == 2:  #  seq from text input
         # incomplete protein seq from text input
         pass
+    elif input == 3:
+        print('enter the root folder location')
+        root_folder = pathlib.Path(input(': ')) # 'data' folder
+        data_set = root_folder/'dataset_catalog.json' # contains the file names
+        data_setdic = json.loads(data_set)
+        data_set_paths = {} # file paths
+        for i in range(len(data_setdic['genes']['files'])):
+            data_set_paths[data_setdic['genes'][files][i]['fileType']] = data_setdic['genes'][files][i]['filePath']
+        # analyse each file (directories stored in data_set_paths)
+        try:
+            with open()
+            # incomplete root folder access
+        except:
+            simpleError()
+            continue
     else:
         simpleError()
         continue
